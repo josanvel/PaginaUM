5141d3fcb12d8b277349550eb3fdd15622b01525
Jose Velez <vlezgmezjosantoniomgoo@hotmail.com>
2015-03-04 01:15:36 -0500

    Pagina Union Misionera

---
+++ CodigoUM/proximamente.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+  <head>
+   <meta  nombre = "ventana"  contenido = "width = device-width, primera escala = 1,0" />
+    <title> Iglesia Uni&oacute;n Misionera </title>
+
+    <link  href = "css/bootstrap/bootstrap.min.css"  rel = "stylesheet"  media = "screen" />
+    <link href="css/bootstrap/bootstrap-responsive.css" rel="stylesheet" />
+    <link href="css/dropdown/dropdown.css" media="screen" rel="stylesheet" type="text/css" />
+    <link href="css/dropdown/themes/nvidia.com/default.advanced.css" media="screen" rel="stylesheet" type="text/css" />
+    
+    <style>
+         body{
+                background: #dbe3e2 url(img/bg.png) fixed repeat top center;
+                overflow-y: scroll;
+                overflow-x: hidden;
+              }
+            /* Tablas
+    -------------------------------------------------- */
+        .tableArea
+        {
+                 border: 0px solid;
+                 width: 98%;
+                 opacity: .90;
+                 filter:alpha(opacity=90); 
+                 background-color: #D7D7D7;
+                 background-repeat: no-repeat; 
+                 box-shadow: 0px 2px 10px #B5C1C5, 0 0 0 2px #EEEEEE inset; 
+        }
+
+        .tablebody
+        {
+             border: 0px solid;
+             background-color: #EEEEEE;
+             opacity: .92;
+             /*background-image: url('imagenes/internas/bt.png') ;    */
+             background-repeat: repeat; 
+             box-shadow: 0px 2px 10px #B5C1C5, 0 0 0 2px #EEEEEE inset; 
+        }
+        
+    </style>
+</head>  
+<body>
+    <table align="center"  width="100%" height="100%" border="0" >
+    
+        <tr>
+            <td width="5%">
+            </td>
+            <td  align="center">
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <img  src="img/proximamente/proximamente.png" />    
+            </td>
+            <td width="5%">
+            </td>
+            
+        </tr>
+        
+    </table>
+</body> 
+</html>  
+
+  
+    
