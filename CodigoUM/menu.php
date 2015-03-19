3d24169646895858a2ae01e5202eb3418b255b45
Leonel <leferami@espol.edu.ec>
2015-03-19 00:03:45 -0500

    menuNuevo

--- CodigoUM/menu.php
+++ CodigoUM/menu.php
@@ -1,96 +1,47 @@
-<table  align="left"  width="80%" height="45px" border="0">
-    <tr>
-        <td>
-              <ul class="dropdown dropdown-horizontal">
-            	<li><a href="noticias.php" class="dir">Inicio</a>
-            	</li>
-            	<li><a href="inicio.php">&iquest;Quienes somos&#63;</a></li>
-            	<li><a  class="dir">Redes</a>
-                  <ul>
-            			<li><a href="#">Redes de Conexi&oacute;n</a>
-                          <ul>
-                              <li> <a href="mujeres.php">Mujeres con Prop&oacute;sito</a></li>
-                              <li> <a href="hombres.php">Hombres con Prop&oacute;sito</a></li>
-                              <li> <a href="proximamente.php">Parejas</a></li>
-                              <li> <a href="proximamente.php">J&oacute;venes y Adolescentes al RESK-T</a></li>
-                              <li> <a href="proximamente.php">Grupos de Conexi&oacute;n</a></li>
-                              <li> <a href="proximamente.php">D&eacute;boras</a></li>
-                              <li> <a href="proximamente.php">Exploradores</a></li>
-                              <li> <a href="proximamente.php">Club vacacional</a></li>
-                              <li> <a href="proximamente.php">Eduka</a>
-                                <ul>
-                                  <li> <a href="proximamente.php">Sala Cuna</a></li>
-                                  <li> <a href="proximamente.php">Casa de Esmeralda</a></li>
-                                
-                                </ul>
-                              </li>
-                          </ul>
-                        </li>
-                        
-                        <li> <a href="#">Redes de Servicio</a>
-                           <ul>
-                                <li> <a href="proximamente.php">Casa de la Mujer</a></li>
-                                <li> <a href="proximamente.php">Fundaci&oacute;n Hombre del ma&ntilde;ana</a></li>
-                                <li> <a href="proximamente.php">Diaconado</a></li>
-                                <li> <a href="proximamente.php">Protocolo</a></li>
-                                <li> <a href="proximamente.php">Misericordia</a></li>
-                           </ul>
-                        </li>
-                        
-                        <li> <a href="#">Redes de Misioneras</a>
-                           <ul>
-                                <li> <a href="./">Casa de Misericordia</a></li>
-                                <li> <a href="./">Casa de Vergeles</a></li>
-                                <li> <a href="./">Casa de Trinitaria</a></li>
-                                <li> <a href="./">19 y Febres C&oacute;rdero</a></li>
-                           </ul>
-                        </li>
-                        
-                        <li> <a href="#">Redes de Artes</a>
-                           <ul>
-                                <li> <a href="./">M&uacute;sica</a></li>
-                                <li> <a href="./">Danza</a></li>
-                                <li> <a href="./">Artes Esc&eacute;nicas</a></li>
-                                
-                           </ul>
-                        </li>
-                        
-                        <li> <a href="#">Redes de Comunicaci&oacute;n</a>
-                           <ul>
-                                <li> <a href="./">Relaciones P&uacute;blicas</a></li>
-                                <li> <a href="./">Sonido e iluminaci&oacute;n</a></li>
-                                <li> <a href="./">Multimedia</a></li>
-                                <li> <a href="./">Visual</a></li>
-                           </ul>
-                           
-                        </li>
-                        
-                        <li> <a href="#">Redes de Financieras</a>
-                           <ul>
-                                <li> <a href="./">Contabilidad</a></li>
-                                <li> <a href="./">Autogesti&oacute;n</a></li>
-                                <li> <a href="./">Secretar&iacute;a</a></li> 
-                           </ul>
-                           
-                        </li>
-                        
-                        <li> <a href="#">Redes de Oraci&oacute;n</a>
-                           <ul>
-                                <li> <a href="./">Intercesi&oacute;n</a></li>
-                                <li> <a href="./">Oraci&oacute;n</a></li>
-                                <li> <a href="./">Profec&iacute;a</a></li> 
-                           </ul>
-                        </li>
-                  </ul>
-            	</li>
-                <li><a  class="dir">Pastores</a></li>
-                <li><a  href="tv_on_line.php">TV/ on-line</a></li>
-                <li><a  class="dir">Conferencias</a></li>
-                <li><a  class="dir">Tienda de art&iacute;culos</a></li>
-            	<li><a href="#">Contacto</a></li>
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle " data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Inicio</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse navbar-ex1-collapse">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">&iquest;Quienes somos&#63;<span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Pastores</a></li>
+
+        <li class="menu-item dropdown " style="position:relative">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Redes <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+                <li><a href="#">Redes de Conexi&oacute;n</a></li>
+                <li><a href="#">Redes de Servicio</a></li>
+                <li><a href="#">Redes de Misioneras</a></li>
+                <li><a href="#">Redes de Artes</a></li>
+                <li class="menu-item dropdown dropdown-submenu">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">Redes de Comunicaci&oacute;n</a>
+                    <ul  class="dropdown-menu">
+                            <li class="menu-item "> <a href="#">Relaciones P&uacute;blicas</a></li>
+                            <li> <a href="#">Sonido e iluminaci&oacute;n</a></li>
+                            <li> <a href="#">Multimedia</a></li>
+                            <li> <a href="#">Visual</a></li>
+                    </ul>
+                </li>
             </ul>
-        </td>
-    
-    </tr>
-  
-</table>
+        </li>
+      </ul>
+      <form class="navbar-form navbar-left" role="search">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
