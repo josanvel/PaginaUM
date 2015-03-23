897d1aa7f15c0f6960ed6263b58d58cab2c9adf2
Jose Velez <vlezgmezjosantoniomgoo@hotmail.com>
2015-03-22 22:34:59 -0500

    Plantilla

---
+++ CodigoUM/plantilla.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include ('templates/Head.php');?>
+</head>
+    <body>
+    <!-- Navbar
+    ============= -->
+    <?php include ('templates/Menu.php');?>
+
+    <!-- Main body
+    ================== -->
+    <div class="wrapper">  
+        
+    </div>
+
+    <!-- Foooter
+    ================== -->
+    <?php include ('templates/Footer.php');?>
+</body>
+</html>
