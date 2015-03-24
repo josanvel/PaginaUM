301cb56de0b93923f1bf39d286e1cddfea780e2d
Leonel <leferami@espol.edu.ec>
2015-03-24 01:58:54 -0500

    olla

--- CodigoUM/mujeres.php
+++ CodigoUM/mujeres.php
@@ -9,12 +9,102 @@
         <?php include ('templates/Menu.php');?>
 
         <div class="container" width="100%">
-            <img src="img/mujeresProposito/mujeres.png"  width="100%" height="40%" />
+            <img src="img/mujeresProposito/mportada.jpg"  width="100%" height="40%" />
         </div>
 
         <!-- Main body
         ================== -->
         <div class="wrapper">  
+
+        	   <!-- Main body
+        ================== -->
+        
+            <div class="container-fluid marco">
+            	<br>
+                <div class="col-xs-12 col-sm-6 col-md-6 ajuste-marco" style="text-align:center" width="100%">
+                    <img src="img/mujeresProposito/mujeres.jpg" width="80%" height="90%" />
+                </div>
+
+                <div class="col-xs-12 col-sm-6 col-md-6 mision centrar historia ajuste-marco">
+                    <br>
+                    	<p>
+                      		Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. 
+                       		Lorem Ipsum ha sido el texto de relleno est&aacute;ndar de las industrias desde el año 1500, cuando un impresor (N. del T.
+                       		persona que se dedica a la imprenta) desconocido us&oacte; una galería de textos y los mezcló de tal manera que logr&oacute; hacer un libro de textos especimen.
+                		</p>
+                </div>
+            </div>
+
+             <br><hr class="featurette-divider" /><br>
+
+            <div class="container-fluid marco">
+
+                <div class="col-xs-12 col-sm-6 col-md-6  mision centrar historia ajuste-marco" style="text-align:center">
+                    <br>
+                    <p>
+                       Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. 
+                       Lorem Ipsum ha sido el texto de relleno est&aacute;ndar de las industrias desde el año 1500, cuando un impresor (N. del T.
+                       persona que se dedica a la imprenta) desconocido us&oacte; una galería de textos y los mezcló de tal manera que logr&oacute; hacer un libro de textos especimen.
+                       </p>
+                </div>
+                
+                <div class="col-xs-12 col-sm-6 col-md-6  ajuste-marco" style="text-align:center " width="100%">
+                    <img src="img/mujeresProposito/mujeres3.jpg" width="80%" height="90%" />
+                </div>
+            </div> 
+
+           <br><hr class="featurette-divider" /><br>
+
+        	<div class="container-fluid marco">
+                <div>
+                    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                        <!-- Indicators -->
+                        <ol class="carousel-indicators">
+                            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                            <li data-target="#myCarousel" data-slide-to="1"></li>
+                        </ol>
+
+                        <!-- Wrapper for slides -->
+                        <div class="carousel-inner" role="listbox">
+                            <div class="item active" style="text-align: center">
+                              <img src="img/mujeresProposito/mujerCarrusel1.jpg" alt="flower">
+                            </div>
+
+                            <div class="item">
+                              <img src="img/mujeresProposito/mujerCarrusel2.jpg" alt="Jose">
+                            </div>
+                        </div>
+
+                        <!-- Left and right controls -->
+                        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                </div>             
+            </div>
+
+           <br><hr class="featurette-divider" /><br>
+
+            <div class="container-fluid marco">
+                <div class="col-xs-12 col-sm-6 col-md-6 ajuste-marco" style="text-align:center" width="100%">
+                    <img src="img/mujeresProposito/horaTE.jpg" width="80%" height="90%" />
+                </div>
+                
+                <div class="col-xs-12 col-sm-6 col-md-6 mision centrar historia ajuste-marco">
+                    <br>
+                    	<p>
+                    	Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. 
+                       Lorem Ipsum ha sido el texto de relleno est&aacute;ndar de las industrias desde el año 1500, cuando un impresor (N. del T.
+                       persona que se dedica a la imprenta) desconocido us&oacte; una galería de textos y los mezcló de tal manera que logr&oacute; hacer un libro de textos especimen.         
+                       <br> </p>
+                </div>
+            </div>
+
             
         </div>
 
